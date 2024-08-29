a422116220997a41dfe154ddcaccfcd4496ad238
Marton Urban <urban.marton@gmail.com>
2024-08-29 11:55:06 +0200

    convert to freezed

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -1,3 +1,8 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'meal.freezed.dart';
+
 enum Complexity {
   simple,
   challenging,
@@ -10,34 +15,21 @@ enum Affordability {
   luxurious,
 }
 
-class Meal {
-  const Meal({
-    required this.id,
-    required this.categories,
-    required this.title,
-    required this.imageUrl,
-    required this.ingredients,
-    required this.steps,
-    required this.duration,
-    required this.complexity,
-    required this.affordability,
-    required this.isGlutenFree,
-    required this.isLactoseFree,
-    required this.isVegan,
-    required this.isVegetarian,
-  });
-
-  final String id;
-  final List<String> categories;
-  final String title;
-  final String imageUrl;
-  final List<String> ingredients;
-  final List<String> steps;
-  final int duration;
-  final Complexity complexity;
-  final Affordability affordability;
-  final bool isGlutenFree;
-  final bool isLactoseFree;
-  final bool isVegan;
-  final bool isVegetarian;
+@freezed
+class Meal with _$Meal {
+  const factory Meal({
+    required String id,
+    required List<String> categories,
+    required String title,
+    required String imageUrl,
+    required List<String> ingredients,
+    required List<String> steps,
+    required int duration,
+    required Complexity complexity,
+    required Affordability affordability,
+    required bool isGlutenFree,
+    required bool isLactoseFree,
+    required bool isVegan,
+    required bool isVegetarian,
+  }) = _Meal;
 }
---
+++ lib/models/meal.freezed.dart
@@ -0,0 +1,447 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'meal.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$Meal {
+  String get id => throw _privateConstructorUsedError;
+  List<String> get categories => throw _privateConstructorUsedError;
+  String get title => throw _privateConstructorUsedError;
+  String get imageUrl => throw _privateConstructorUsedError;
+  List<String> get ingredients => throw _privateConstructorUsedError;
+  List<String> get steps => throw _privateConstructorUsedError;
+  int get duration => throw _privateConstructorUsedError;
+  Complexity get complexity => throw _privateConstructorUsedError;
+  Affordability get affordability => throw _privateConstructorUsedError;
+  bool get isGlutenFree => throw _privateConstructorUsedError;
+  bool get isLactoseFree => throw _privateConstructorUsedError;
+  bool get isVegan => throw _privateConstructorUsedError;
+  bool get isVegetarian => throw _privateConstructorUsedError;
+
+  /// Create a copy of Meal
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $MealCopyWith<Meal> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MealCopyWith<$Res> {
+  factory $MealCopyWith(Meal value, $Res Function(Meal) then) =
+      _$MealCopyWithImpl<$Res, Meal>;
+  @useResult
+  $Res call(
+      {String id,
+      List<String> categories,
+      String title,
+      String imageUrl,
+      List<String> ingredients,
+      List<String> steps,
+      int duration,
+      Complexity complexity,
+      Affordability affordability,
+      bool isGlutenFree,
+      bool isLactoseFree,
+      bool isVegan,
+      bool isVegetarian});
+}
+
+/// @nodoc
+class _$MealCopyWithImpl<$Res, $Val extends Meal>
+    implements $MealCopyWith<$Res> {
+  _$MealCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of Meal
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? categories = null,
+    Object? title = null,
+    Object? imageUrl = null,
+    Object? ingredients = null,
+    Object? steps = null,
+    Object? duration = null,
+    Object? complexity = null,
+    Object? affordability = null,
+    Object? isGlutenFree = null,
+    Object? isLactoseFree = null,
+    Object? isVegan = null,
+    Object? isVegetarian = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      categories: null == categories
+          ? _value.categories
+          : categories // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      ingredients: null == ingredients
+          ? _value.ingredients
+          : ingredients // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      steps: null == steps
+          ? _value.steps
+          : steps // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      duration: null == duration
+          ? _value.duration
+          : duration // ignore: cast_nullable_to_non_nullable
+              as int,
+      complexity: null == complexity
+          ? _value.complexity
+          : complexity // ignore: cast_nullable_to_non_nullable
+              as Complexity,
+      affordability: null == affordability
+          ? _value.affordability
+          : affordability // ignore: cast_nullable_to_non_nullable
+              as Affordability,
+      isGlutenFree: null == isGlutenFree
+          ? _value.isGlutenFree
+          : isGlutenFree // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isLactoseFree: null == isLactoseFree
+          ? _value.isLactoseFree
+          : isLactoseFree // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isVegan: null == isVegan
+          ? _value.isVegan
+          : isVegan // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isVegetarian: null == isVegetarian
+          ? _value.isVegetarian
+          : isVegetarian // ignore: cast_nullable_to_non_nullable
+              as bool,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$MealImplCopyWith<$Res> implements $MealCopyWith<$Res> {
+  factory _$$MealImplCopyWith(
+          _$MealImpl value, $Res Function(_$MealImpl) then) =
+      __$$MealImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String id,
+      List<String> categories,
+      String title,
+      String imageUrl,
+      List<String> ingredients,
+      List<String> steps,
+      int duration,
+      Complexity complexity,
+      Affordability affordability,
+      bool isGlutenFree,
+      bool isLactoseFree,
+      bool isVegan,
+      bool isVegetarian});
+}
+
+/// @nodoc
+class __$$MealImplCopyWithImpl<$Res>
+    extends _$MealCopyWithImpl<$Res, _$MealImpl>
+    implements _$$MealImplCopyWith<$Res> {
+  __$$MealImplCopyWithImpl(_$MealImpl _value, $Res Function(_$MealImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of Meal
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? categories = null,
+    Object? title = null,
+    Object? imageUrl = null,
+    Object? ingredients = null,
+    Object? steps = null,
+    Object? duration = null,
+    Object? complexity = null,
+    Object? affordability = null,
+    Object? isGlutenFree = null,
+    Object? isLactoseFree = null,
+    Object? isVegan = null,
+    Object? isVegetarian = null,
+  }) {
+    return _then(_$MealImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      categories: null == categories
+          ? _value._categories
+          : categories // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      ingredients: null == ingredients
+          ? _value._ingredients
+          : ingredients // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      steps: null == steps
+          ? _value._steps
+          : steps // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      duration: null == duration
+          ? _value.duration
+          : duration // ignore: cast_nullable_to_non_nullable
+              as int,
+      complexity: null == complexity
+          ? _value.complexity
+          : complexity // ignore: cast_nullable_to_non_nullable
+              as Complexity,
+      affordability: null == affordability
+          ? _value.affordability
+          : affordability // ignore: cast_nullable_to_non_nullable
+              as Affordability,
+      isGlutenFree: null == isGlutenFree
+          ? _value.isGlutenFree
+          : isGlutenFree // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isLactoseFree: null == isLactoseFree
+          ? _value.isLactoseFree
+          : isLactoseFree // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isVegan: null == isVegan
+          ? _value.isVegan
+          : isVegan // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isVegetarian: null == isVegetarian
+          ? _value.isVegetarian
+          : isVegetarian // ignore: cast_nullable_to_non_nullable
+              as bool,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$MealImpl with DiagnosticableTreeMixin implements _Meal {
+  const _$MealImpl(
+      {required this.id,
+      required final List<String> categories,
+      required this.title,
+      required this.imageUrl,
+      required final List<String> ingredients,
+      required final List<String> steps,
+      required this.duration,
+      required this.complexity,
+      required this.affordability,
+      required this.isGlutenFree,
+      required this.isLactoseFree,
+      required this.isVegan,
+      required this.isVegetarian})
+      : _categories = categories,
+        _ingredients = ingredients,
+        _steps = steps;
+
+  @override
+  final String id;
+  final List<String> _categories;
+  @override
+  List<String> get categories {
+    if (_categories is EqualUnmodifiableListView) return _categories;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_categories);
+  }
+
+  @override
+  final String title;
+  @override
+  final String imageUrl;
+  final List<String> _ingredients;
+  @override
+  List<String> get ingredients {
+    if (_ingredients is EqualUnmodifiableListView) return _ingredients;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_ingredients);
+  }
+
+  final List<String> _steps;
+  @override
+  List<String> get steps {
+    if (_steps is EqualUnmodifiableListView) return _steps;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_steps);
+  }
+
+  @override
+  final int duration;
+  @override
+  final Complexity complexity;
+  @override
+  final Affordability affordability;
+  @override
+  final bool isGlutenFree;
+  @override
+  final bool isLactoseFree;
+  @override
+  final bool isVegan;
+  @override
+  final bool isVegetarian;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'Meal(id: $id, categories: $categories, title: $title, imageUrl: $imageUrl, ingredients: $ingredients, steps: $steps, duration: $duration, complexity: $complexity, affordability: $affordability, isGlutenFree: $isGlutenFree, isLactoseFree: $isLactoseFree, isVegan: $isVegan, isVegetarian: $isVegetarian)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'Meal'))
+      ..add(DiagnosticsProperty('id', id))
+      ..add(DiagnosticsProperty('categories', categories))
+      ..add(DiagnosticsProperty('title', title))
+      ..add(DiagnosticsProperty('imageUrl', imageUrl))
+      ..add(DiagnosticsProperty('ingredients', ingredients))
+      ..add(DiagnosticsProperty('steps', steps))
+      ..add(DiagnosticsProperty('duration', duration))
+      ..add(DiagnosticsProperty('complexity', complexity))
+      ..add(DiagnosticsProperty('affordability', affordability))
+      ..add(DiagnosticsProperty('isGlutenFree', isGlutenFree))
+      ..add(DiagnosticsProperty('isLactoseFree', isLactoseFree))
+      ..add(DiagnosticsProperty('isVegan', isVegan))
+      ..add(DiagnosticsProperty('isVegetarian', isVegetarian));
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$MealImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            const DeepCollectionEquality()
+                .equals(other._categories, _categories) &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.imageUrl, imageUrl) ||
+                other.imageUrl == imageUrl) &&
+            const DeepCollectionEquality()
+                .equals(other._ingredients, _ingredients) &&
+            const DeepCollectionEquality().equals(other._steps, _steps) &&
+            (identical(other.duration, duration) ||
+                other.duration == duration) &&
+            (identical(other.complexity, complexity) ||
+                other.complexity == complexity) &&
+            (identical(other.affordability, affordability) ||
+                other.affordability == affordability) &&
+            (identical(other.isGlutenFree, isGlutenFree) ||
+                other.isGlutenFree == isGlutenFree) &&
+            (identical(other.isLactoseFree, isLactoseFree) ||
+                other.isLactoseFree == isLactoseFree) &&
+            (identical(other.isVegan, isVegan) || other.isVegan == isVegan) &&
+            (identical(other.isVegetarian, isVegetarian) ||
+                other.isVegetarian == isVegetarian));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      id,
+      const DeepCollectionEquality().hash(_categories),
+      title,
+      imageUrl,
+      const DeepCollectionEquality().hash(_ingredients),
+      const DeepCollectionEquality().hash(_steps),
+      duration,
+      complexity,
+      affordability,
+      isGlutenFree,
+      isLactoseFree,
+      isVegan,
+      isVegetarian);
+
+  /// Create a copy of Meal
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$MealImplCopyWith<_$MealImpl> get copyWith =>
+      __$$MealImplCopyWithImpl<_$MealImpl>(this, _$identity);
+}
+
+abstract class _Meal implements Meal {
+  const factory _Meal(
+      {required final String id,
+      required final List<String> categories,
+      required final String title,
+      required final String imageUrl,
+      required final List<String> ingredients,
+      required final List<String> steps,
+      required final int duration,
+      required final Complexity complexity,
+      required final Affordability affordability,
+      required final bool isGlutenFree,
+      required final bool isLactoseFree,
+      required final bool isVegan,
+      required final bool isVegetarian}) = _$MealImpl;
+
+  @override
+  String get id;
+  @override
+  List<String> get categories;
+  @override
+  String get title;
+  @override
+  String get imageUrl;
+  @override
+  List<String> get ingredients;
+  @override
+  List<String> get steps;
+  @override
+  int get duration;
+  @override
+  Complexity get complexity;
+  @override
+  Affordability get affordability;
+  @override
+  bool get isGlutenFree;
+  @override
+  bool get isLactoseFree;
+  @override
+  bool get isVegan;
+  @override
+  bool get isVegetarian;
+
+  /// Create a copy of Meal
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$MealImplCopyWith<_$MealImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
