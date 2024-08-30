9cea710d14626b875b746339c6f0cee992547872
Marton Urban <urban.marton@gmail.com>
2024-08-30 09:02:21 +0200

    use code generation instead

--- lib/providers/meals_provider.dart
+++ lib/providers/meals_provider.dart
@@ -1,7 +1,14 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import '../data/dummy_data.dart';
+import '../models/meal.dart';
 
-final mealsProvider = Provider((ref) {
+part 'meals_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+List<Meal> meals(MealsRef ref) {
   return dummyMeals;
-});
+}
+
+// final mealsProvider = Provider((ref) {
+//   return dummyMeals;
+// });
---
+++ lib/providers/meals_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'meals_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$mealsHash() => r'380d9b646fcf18b2e727e7f74b5c4a1c72dfa67c';
+
+/// See also [meals].
+@ProviderFor(meals)
+final mealsProvider = Provider<List<Meal>>.internal(
+  meals,
+  name: r'mealsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$mealsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef MealsRef = ProviderRef<List<Meal>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
